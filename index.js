93dd60b5a186df05aed0d4bd5cddb0c023890b51
+ <ronoh.ivy@gmail.com>
2022-06-19 17:56:28 +0300

    commit tree 40655804cc00749fe7dd31b3f7a4940271cf6262

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // iterate from the beginning of the string to the middle of the string
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    // compare the letter we're iterating over to the corresponding letter at the end of the string
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      // if the letters don't match, return false
+      return false;
+    }
+  }
+
+  // if we reach the middle, and all the letters match, return true
+  return true;
+
 }
 
 /* 
   Add your pseudocode here
+  Go through the letters one by one from either side of the string.
+  Compare the letters on each end
+  If they are similar, move one step towards the middle on either side.
+  If they are similar up till the middle, return true. Else, return false.
+
 */
 
 /*
   Add written explanation of your solution here
+  r a c e c a r
+0 1 2 3 4 5 6
+i           j
+r a c e c a r
+0 1 2 3 4 5 6
+  i       j
+r a c e c a r
+0 1 2 3 4 5 6
+    i   j
+r a c e c a r
+0 1 2 3 4 5 6
+      ij
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +50,30 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
+
